fd4b9708ed8341c75caa9c702b3d8841562db4f8
Eye-Mans <laluiman468@gmail.com>
2023-12-30 07:10:46 +0800

    commit

--- Emoji/ContentView.swift
+++ Emoji/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable{
+    case 😀, 🤓, 🤣, 🥳, 😰, 🤩, 😱, 😇, 🙂,
+         😉, 🥰, 🥸, 😫, 🥺, 😡, 😭, 😚, 😋,
+         🤪, 😎, 😁, 😶‍🌫️, 🤨, 🧐, 🤬, 🤭, 🫣,
+         🤥, 🤗, 🤯, 🤔, 🫨, 🫡, 🤫, 😥, 🫠
+}
+
+
 struct ContentView: View {
+    
+    @State var selection: Emoji = .😉
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView{
+            VStack {
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(
+                            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 350, height: 250)
+                        .shadow(radius: 20)
+                        
+                    Text(selection.rawValue)
+                        .font(.system(size: 150))
+                }
+                .padding()
+                Text("Spin the Wheel to see Emoji!")
+                    .fontWeight(.semibold)
+                Picker("Spin Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self){ emoji in
+                        Text(emoji.rawValue)
+                            .font(.system(size: 30))
+                    }
+                }
+                .pickerStyle(.wheel)
+                .padding()
+            }
+            .navigationTitle("Emoji Wheel")
+            .padding()
+            
         }
-        .padding()
     }
 }
 
