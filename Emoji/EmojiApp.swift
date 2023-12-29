f090cc9cd7ffe81a7cb25054d9a45990493ee52d
Eye-Mans <laluiman468@gmail.com>
2023-12-29 11:57:33 +0800

    Initial Commit

---
+++ Emoji/EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiApp.swift
+//  Emoji
+//
+//  Created by Lalu Iman Abdullah on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
